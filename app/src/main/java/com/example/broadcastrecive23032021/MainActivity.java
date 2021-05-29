48e6f5145745c161153b383cb0b995fed5610776
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-05-29 21:20:10 +0700

    finish

--- app/src/main/java/com/example/broadcastrecive23032021/MainActivity.java
+++ app/src/main/java/com/example/broadcastrecive23032021/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.broadcastrecive23032021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -13,23 +14,28 @@ import org.greenrobot.eventbus.ThreadMode;
 public class MainActivity extends AppCompatActivity {
 
     TextView mTvAirPlaneMode;
+    MyBroadCast mMyBroadCast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mTvAirPlaneMode = findViewById(R.id.textView);
+        mMyBroadCast = new MyBroadCast();
     }
 
     @Override
     public void onStart() {
         super.onStart();
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        registerReceiver(mMyBroadCast,intentFilter);
         EventBus.getDefault().register(this);
     }
 
     @Override
     public void onStop() {
         super.onStop();
+        unregisterReceiver(mMyBroadCast);
         EventBus.getDefault().unregister(this);
     }
 
